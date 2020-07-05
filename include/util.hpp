e30be37e8b277be12ee1c6776664565cd5473a85
Shunsuke Kanda <shnsk.knd@gmail.com>
2020-07-05 10:43:41 +0900

    modify to output json results

--- include/util.hpp
+++ include/util.hpp
@@ -33,10 +33,28 @@ uint64_t rand_u64() {
     return z ^ (z >> 31);
 }
 
-uint64_t create_random_bits(std::vector<uint64_t>& bits, uint64_t threshold) {
+// From http://xoroshiro.di.unimi.it/splitmix64.c
+class splitmix64 {
+public:
+    splitmix64(uint64_t seed) : x(seed){};
+
+    uint64_t next() {
+        uint64_t z = (x += uint64_t(0x9E3779B97F4A7C15));
+        z = (z ^ (z >> 30)) * uint64_t(0xBF58476D1CE4E5B9);
+        z = (z ^ (z >> 27)) * uint64_t(0x94D049BB133111EB);
+        return z ^ (z >> 31);
+    }
+
+private:
+    uint64_t x;
+};
+
+uint64_t create_random_bits(std::vector<uint64_t>& bits, uint64_t threshold,
+                            uint64_t seed = 13) {
     uint64_t num_ones = 0;
+    splitmix64 hasher(seed);
     for (uint64_t i = 0; i < bits.size() * 64; i++) {
-        if (rand_u64() < threshold) {
+        if (hasher.next() < threshold) {
             bits[i / 64] |= 1ULL << (i % 64);
             num_ones++;
         }
