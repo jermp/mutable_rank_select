20c24cbac59a8b2ce23861b640eae5aa7854cd0a
Giulio Ermanno Pibiri <jeis90@gmail.com>
2020-09-17 10:37:09 +0200

    modified benchmarks for prefix_sum256/512: let n vary

--- src/perf_prefix_sum512.cpp
+++ src/perf_prefix_sum512.cpp
@@ -12,20 +12,20 @@
 using namespace dyrs;
 
 static constexpr int runs = 100;
-static constexpr uint32_t num_queries = 1000000;
+static constexpr uint32_t num_queries = 10000;
 static constexpr uint64_t bits_seed = 13;
 static constexpr uint64_t query_seed = 71;
 static constexpr double density = 0.3;
 
-static constexpr std::array<uint64_t, 1> sizes = {
-    1ULL << 9,
-};
-// static constexpr std::array<uint64_t, 24> sizes = {
-//     1ULL << 9,  1ULL << 10, 1ULL << 11, 1ULL << 12, 1ULL << 13, 1ULL << 14,
-//     1ULL << 15, 1ULL << 16, 1ULL << 17, 1ULL << 18, 1ULL << 19, 1ULL << 20,
-//     1ULL << 21, 1ULL << 22, 1ULL << 23, 1ULL << 24, 1ULL << 25, 1ULL << 26,
-//     1ULL << 27, 1ULL << 28, 1ULL << 29, 1ULL << 30, 1ULL << 31, 1ULL << 32,
+// static constexpr std::array<uint64_t, 1> sizes = {
+//     1ULL << 9,
 // };
+static constexpr std::array<uint64_t, 24> sizes = {
+    1ULL << 9,  1ULL << 10, 1ULL << 11, 1ULL << 12, 1ULL << 13, 1ULL << 14,
+    1ULL << 15, 1ULL << 16, 1ULL << 17, 1ULL << 18, 1ULL << 19, 1ULL << 20,
+    1ULL << 21, 1ULL << 22, 1ULL << 23, 1ULL << 24, 1ULL << 25, 1ULL << 26,
+    1ULL << 27, 1ULL << 28, 1ULL << 29, 1ULL << 30, 1ULL << 31, 1ULL << 32,
+};
 
 template <prefixsum_modes>
 inline uint64_t prefixsum_512(const uint64_t*, uint64_t) {
@@ -75,6 +75,7 @@ inline uint64_t prefixsum_512<prefixsum_modes::parallel>(const uint64_t* x,
 template <prefixsum_modes Mode>
 void test(std::string type) {
     std::vector<uint64_t> bits(sizes.back() / 64);
+    std::vector<std::pair<const uint64_t*, uint64_t>> queries(num_queries);
     auto num_ones = create_random_bits(bits, UINT64_MAX * density, bits_seed);
     std::cout << "# number of ones: " << num_ones << " ("
               << num_ones / double(sizes.back()) << ")" << std::endl;
@@ -87,8 +88,6 @@ void test(std::string type) {
         splitmix64 hasher(query_seed);
 
         const auto num_buckets = n / 512;
-        std::vector<std::pair<const uint64_t*, uint64_t>> queries(num_queries);
-
         for (uint64_t i = 0; i < num_queries; i++) {
             const uint64_t* x = &bits[(hasher.next() % num_buckets) * 512 / 64];
             queries[i] = {x, hasher.next() % 8};
